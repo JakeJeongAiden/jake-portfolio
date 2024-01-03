304eeaa745592655ee99b97ba620bf41d4741bc3
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2024-01-02 18:37:13 -0800

    first page finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,14 +25,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-zinc-50 text-zinc-950 ${myFont.className} h-[5000px]`}
+        className={`bg-zinc-50 text-zinc-950 ${myFont.className} h-[5000px] pt-28 sm:pt-36`}
       >
         {/* Left & Right side of the element. */}
         <div className="absolute inset-0 h-[5000px] w-full -z-10 bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
-        <div className="bg-[#e7e5e4] absolute top-[-1rem] -z-10 opacity-10 left-[-39rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#e4e4e7] absolute top-[-1rem] -z-10 opacity-50 left-[-39rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
         <Header />
-        <About />
+
         {children}
       </body>
     </html>
--- components/intro.tsx
+++ components/intro.tsx
@@ -10,16 +10,14 @@ import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section
-      id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
-    >
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
+              // tween is smooth non bouncing animation.
               type: "tween",
               duration: 0.2,
             }}
@@ -39,7 +37,9 @@ export default function Intro() {
             className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
+            // transition is optional but it's recommended to use it for better user experience.
             transition={{
+              // spring is bouncy animation.
               type: "spring",
               stiffness: 125,
               delay: 0.1,
@@ -50,6 +50,65 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <p className="font-bold uppercase leading-10 text-4xl text-zinc-400">
+          Jake Jeong
+        </p>{" "}
+        <span>A </span>
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">experiences </span> in{" "}
+        <span className="font-bold underline">Next.js</span>,{" "}
+        <span className="font-bold underline">Javascript</span>,{" "}
+        <span className="font-bold underline">TailwindCSS</span>,{" "}
+      </motion.h1>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2 }}
+        className="flex flex-col sm:flex-row justify-center gap-3 px-4 text-lg font-medium"
+      >
+        <Link
+          href="#contact"
+          // active:scale-105 to make it look like it's being pressed.
+          className="group bg-zinc-900 text-zinc-50 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-zinc-950 active:scale-105 transition cursor-pointer"
+        >
+          {/* de-emphasizing icons next to texts */}
+          Contact Me Here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        {/* Using anchor tag because we are downloading something */}
+        <a
+          // in nextjs you can use /public folder to store static files.
+          href="/CV.pdf"
+          download
+          className="group bg-zinc-100 text-zinc-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-zinc-100 active:scale-105 transition cursor-pointer border-black/20"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        {/* Using anchor tag because we are navigating to actual link */}
+
+        <a
+          href="https://www.linkedin.com/in/jake-jeong-801438148/"
+          target="_blank"
+          className="bg-zinc-100 text-zinc-700 p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-zinc-100 hover:text-zinc-900 active:scale-105 transition cursor-pointer border-black/20"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/JakeJeongAiden"
+          target="_blank"
+          className="bg-zinc-100 text-zinc-700 p-4 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-zinc-900  hover:bg-zinc-100 active:scale-105 transition cursor-pointer border-black/20"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
