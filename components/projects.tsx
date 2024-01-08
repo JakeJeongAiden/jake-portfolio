af2d99be8c7e0b63fb0d8040f5925f40690b9161
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2024-01-07 22:58:38 -0800

    Add fully working header

---
+++ components/projects.tsx
@@ -0,0 +1,52 @@
+"use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { projectsData } from "@/lib/data";
+import SectionHeading from "@/components/section-heading";
+
+export default function Projects() {
+  return (
+    <>
+      <SectionHeading>Project</SectionHeading>
+
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full max-w-sm"
+      >
+        <CarouselContent>
+          {/* {Array.from({ length: 5 }).map((_, index) =>  */}
+          {projectsData.map((project, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card className=" ">
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold">
+                      {project.title}
+                    </span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
+  );
+}
