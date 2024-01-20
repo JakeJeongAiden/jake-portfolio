8a96685204afe4b0e9ef87af01a77ee0649c566a
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2024-01-20 12:06:06 -0800

    remove svg mask effect

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -9,7 +9,6 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/app/lib/hooks";
 import { useActiveSectionContext } from "@/app/context/active-section-context";
-import { MaskContainer } from "@/app/components/ui/svg-mask-effect";
 
 /* ================= Note =================
 
@@ -20,11 +19,7 @@ export default function Intro() {
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
-    <section
-      ref={ref}
-      id="home"
-      className="mb-28 text-center sm:mb-0 scroll-mt-[100rem]"
-    >
+    <section ref={ref} id="home" className="mb-28  sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -36,7 +31,7 @@ export default function Intro() {
               duration: 0.2,
             }}
           >
-            <Image
+            {/* <Image
               src="/profile-picture.jpg"
               alt="Jake Jeong's Portrait picture"
               width="500"
@@ -44,7 +39,7 @@ export default function Intro() {
               quality="95"
               priority={true}
               className="h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
+            /> */}
           </motion.div>
 
           {/* <motion.span
@@ -64,72 +59,55 @@ export default function Intro() {
           </motion.span> */}
         </div>
       </div>
-      <section className="flex flex-col">
-        <motion.h1
-          className="flex justify-start items-baseline mt-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-        >
-          <MaskContainer
-            size={20}
-            //
-            revealText={
-              <p className=" mx-auto text-slate-800 text-center  text-4xl font-bold">
-                <span className="font-extrabold tracking-tight leading-10 text-4xl sm:text-8xl text-zinc-200 dark:text-gray-950 mb-2">
-                  Jake Jeong
-                </span>{" "}
-                <span className="font-light pl-2 ">.</span>
-                <span className="tracking-wide font-semibold text-lg sm:text-2xl pr-44 leading-1">
-                  A Full-Stack Developer
-                </span>
-              </p>
-            }
-            className="h-[30rem] border border-black/0 rounded-md"
-          >
-            <p className="flexjustify-start mx-auto text-slate-800 text-center  text-4xl font-bold">
-              <span className="font-extrabold tracking-tight leading-10 text-4xl sm:text-8xl text-zinc-200 dark:text-gray-950 mb-2">
-                Jake Jeong
-              </span>{" "}
-              <span className="font-light pl-2 ">.</span>
-              <span className="tracking-wide font-semibold text-lg sm:text-2xl pr-44 leading-1">
-                A Full-Stack Developer
-              </span>
-            </p>
-          </MaskContainer>
-        </motion.h1>
-
-        <motion.div
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          className="flex justify-end mt-6 pb-36 text-md font-thin !leading-[1.5] sm:text-xl "
-        >
+      <motion.h1
+        className="flex justify-start items-baseline mt-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ ease: "easeOut" }}
+      >
+        <p className="mx-auto text-zinc-800 text-center  text-4xl font-bold">
+          <span className="font-extrabold tracking-tight leading-10 text-4xl sm:text-8xl text-zinc-200 dark:text-gray-950 mb-2">
+            Jake Jeong
+          </span>{" "}
+          <span className="font-light pl-2 ">.</span>
+          <span className="tracking-wide font-semibold text-lg sm:text-2xl pr-44 leading-1 text-zinc-700">
+            A Full-Stack Developer
+          </span>
+        </p>
+      </motion.h1>
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ ease: "easeOut" }}
+        className="flex justify-end mt-6 pb-36 text-md font-thin !leading-[1.5] sm:text-xl text-zinc-800"
+      >
+        <div>
           <span className="">creative and experienced with</span>{" "}
           <span className="font-normal">Next.js</span>,{" "}
           <span className="font-normal ">Typescript</span>,{" "}
           <span className="font-normal ">TailwindCSS</span>.{" "}
           <span className="font-thin ">and more...</span>.{" "}
-        </motion.div>
-      </section>
+        </div>
+      </motion.div>
 
+      {/* <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ ease: "easeOut" }}
+          className="flex justify-end mt-6 pb-36 text-md font-thin !leading-[1.5] sm:text-xl "
+        >
+          <span className="">c``reative and experienced with</span>{" "}
+          <span className="font-normal">Next.js</span>,{" "}
+          <span className="font-normal ">Typescript</span>,{" "}
+          <span className="font-normal ">TailwindCSS</span>.{" "}
+          <span className="font-thin ">and more...</span>.{" "}
+        </motion.div> */}
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.2 }}
         className="flex flex-col sm:flex-row justify-center gap-3 px-4 text-lg font-medium"
       >
-        <Link
-          href="#contact"
-          // active:scale-105 to make it look like it's being pressed.
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection("Contact");
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          {/* de-emphasizing icons next to texts */}
-          Contact Me Here{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
         {/* Using anchor tag because we are downloading something */}
 
         <a
